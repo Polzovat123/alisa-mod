6a46ae728288e9484ca7300c543feeb239e7069f
Polzovat123 <42420479+Polzovat123@users.noreply.github.com>
2019-08-07 16:08:34 +0300

    Update alisa.py

--- alisa.py
+++ alisa.py
@@ -19,6 +19,7 @@ def echo():
             'session':request.json['session'],
             'response':{
               'text':  'Спасаев в пути ... Назовите слово'
+               'tts':''
             }
         }
     else:
