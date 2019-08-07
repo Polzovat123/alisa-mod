f64ede615c43a925cc4510e2e405193980909d7a
Polzovat123 <42420479+Polzovat123@users.noreply.github.com>
2019-08-07 14:11:40 +0300

    Add files via upload

---
+++ alisa.py
@@ -0,0 +1,36 @@
+from flask import Flask,  request
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def echo():
+    
+    if request.json['request']['original_utterance'] == '':
+        response = {
+            'version':request.json['version'],
+            'session':request.json['sesion'],
+            'response':{
+              'text':  'Спасаев в пути ... Назовите слово'
+            }
+        }
+    else:
+        if request.json['request']['original_utterance'] == 'шалаш':
+            response = {
+                'version':request.json['version'],
+                'session':request.json['sesion'],
+                'response':{
+                  'text':  'True. Спасаев в пути ... Назовите слово'
+                }
+            }
+        else:
+            response = {
+                'version':request.json['version'],
+                'session':request.json['sesion'],
+                'response':{
+                  'text':  'Fak. Спасаев в пути ... Назовите слово'
+                }
+            }
+    return response
+
+app.run(host='0.0.0.0', port=os.getenv('PORT',5000))
